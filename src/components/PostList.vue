ab8cb3da6c3add46b172401d97be336a4330cb70
Lena Pavlenko <pawlencko.alena2010@yandex.ru>
2024-05-03 19:11:04 +0300

    удалить пост, сохранение в localestorage

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,18 +1,31 @@
 <template>
-    <div class="post-box">
-        <div class="post-item" v-for="post in posts">
-            <h2 class="post-item__title">{{ post.title }}</h2>
-            <p class="post-item__desc">{{ post.description }}</p>
-        </div>
-    </div>
+  <div class="post-box">
+    <post-item v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)" />
+  </div>
 </template>
 
 <script>
-    export default {
-        
+import PostItem from '@/components/PostItem'
+
+export default {
+  components: {
+    PostItem
+  },
+  props: {
+    posts: {
+      type: Array,
+      required: true,
     }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-
-</style> 
+  .post-box {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    max-width: 1000px;
+    width: 100%;
+  }
+</style>
